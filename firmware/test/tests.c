36e4e757855b1a794e25b4aead16f251998ec3a1
gukov <gukov@mri-progress.ru>
2021-08-11 10:03:24 +0300

    mod: тест

--- firmware/test/tests.c
+++ firmware/test/tests.c
@@ -4,19 +4,16 @@
 void test_streams()
 {
     const int8_t noStreams = 8;
-    REG(0xA0000000) = 1;
 
     for (uint32_t i = 0; i < 0x10*noStreams; ++i) {
         REG(BA_MEM_DMA+i) = i;
     }
 
     DmaStream stream[noStreams];
-    for (int i = 1; i < noStreams; ++i) {
+    for (int i = 0; i < noStreams; ++i) {
         dma_init(&stream[i], i, 0x10000000*i, 0x10*i, dirMemToMem, 16);
     }
-    for (int i = 1; i < noStreams; ++i) {
+    for (int i = 0; i < noStreams; ++i) {
         dma_config(&stream[i]);
     }
-
-    REG(0xA0000000) = 2;
 }
